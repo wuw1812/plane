6a5c4499289f3281b4e6ea9da7f82db962118009
wuw1812 <114126739+wuw1812@users.noreply.github.com>
2022-10-06 11:58:51 +0800

    Add files via upload

---
+++ plane.py
@@ -0,0 +1,124 @@
+import pygame
+import random
+import os
+pygame.init()
+screen = pygame.display.set_mode((500,600))
+running=True
+clock = pygame.time.Clock()
+
+background_img = pygame.image.load(os.path.join("img","background.png")).convert()
+player_img = pygame.image.load(os.path.join("img","player.png")).convert()
+rock_img = pygame.image.load(os.path.join("img","rock.png")).convert()
+bullet_img = pygame.image.load(os.path.join("img","bullet.png")).convert()
+
+FPS = 60
+GREEN= (0,255,0)
+BLACK= (0,0,0)
+RED= (255,0,0)
+YELLOW= (255,255,0)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(player_img,(50,38))
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = 250
+        self.rect.bottom = 600 - 10
+        self.speedx = 8
+    def update(self):
+        key_pressed = pygame.key.get_pressed()
+        if key_pressed[pygame.K_d]:
+            self.rect.x+=self.speedx
+        if key_pressed[pygame.K_a]:
+            self.rect.x-=self.speedx
+
+        if self.rect.right > 500:
+            self.rect.right = 500
+        if self.rect.left < 0:
+            self.rect.left = 0
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
+
+
+class Rock(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = rock_img
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(0,500 - self.rect.width)
+        self.rect.y = random.randrange(-100,-40)
+        self.speedy = random.randrange(2,10)
+        self.speedx = random.randrange(-3,3)
+        
+    def update(self):
+        self.rect.y += self.speedy
+        self.rect.x += self.speedx
+        if self.rect.top > 600 or self.rect.left > 500 or self.rect.right < 0:
+            self.rect.x = random.randrange(0,500 - self.rect.width)
+            self.rect.y = random.randrange(-100,-40)
+            self.speedy = random.randrange(2,10)
+            self.speedx = random.randrange(-3,3)
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = bullet_img
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.bottom = y
+        self.speedy = -10
+        
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.bottom < 0:
+            self.kill()
+
+
+
+
+all_sprites = pygame.sprite.Group()        
+rocks = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+for i in range(8):
+    r= Rock()
+    all_sprites.add(r)
+    rocks.add(r)
+
+
+while running:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shoot()
+    
+    
+
+    
+    all_sprites.update()
+    hits = pygame.sprite.groupcollide(rocks,bullets,True,True)
+    for hit in hits:
+        r = Rock()
+        all_sprites.add(r)
+        rocks.add(r)
+    
+    hits = pygame.sprite.spritecollide(player,rocks,False)
+    if hits:
+        running = False
+    
+    screen.fill(BLACK)
+    screen.blit(background_img,(0,0))
+    all_sprites.draw(screen)
+    
+    pygame.display.update()
+pygame.quit()
